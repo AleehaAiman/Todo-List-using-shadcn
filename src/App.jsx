adf897c69cfc28179f3513112af9bc1395e93dc1
Aleeha Aiman <131415715+AleehaAiman@users.noreply.github.com>
2024-09-25 11:33:21 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Playground } from './Components/component/playground';
+import "../src/App.css";
+function App() {
+    return (
+      
+        <Playground />
+    )
+}
+
+export default App
